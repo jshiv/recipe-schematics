0ec029f9ee88871d9eae2b1975f7895b48b587c6
Jason Shiverick <jshiv00@gmail.com>
2014-08-11 15:12:24 -0700

    added template derictory and image directory

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    # return 'Hello World!'
+    return render_template('hello.html')
 
