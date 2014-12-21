be047f08747a8bfc923741127d1e121d2a0d8026
Jason Shiverick <jshiv00@gmail.com>
2014-12-20 16:57:16 -0800

    hello worold

--- hello.py
+++ hello.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # return 'Hello World!'
-    return render_template('hello.html')
+    return 'Hello World!'
+    #return render_template('hello.html')
 
 if __name__ == "__main__":
     app.run()
